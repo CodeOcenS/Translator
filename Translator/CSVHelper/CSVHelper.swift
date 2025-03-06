de18c84df6674362a12c04f40aaf7efae7c30ad4
CodeOcens <2636015706@qq.com>
2025-03-06 14:33:21 +0800

    feat: 添加测试数据。

---
+++ Translator/CSVHelper/CSVHelper.swift
@@ -0,0 +1,9 @@
+//
+//  CSVHelper.swift
+//  Translator
+//
+//  Created by PandaEye on 2025/3/6.
+//  Copyright © 2025 PandaEye. All rights reserved.
+//
+
+import Foundation
---
+++ Translator/Classes/HandleError.swift
@@ -0,0 +1,9 @@
+//
+//  Error.swift
+//  Translator
+//
+//  Created by PandaEye on 2025/2/28.
+//  Copyright © 2025 PandaEye. All rights reserved.
+//
+
+import Foundation
